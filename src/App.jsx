a535ba060a1b49f09699a187005aa8f8361815bc
ShravaniSnaik <shravanin5270652@gmail.com>
2025-08-01 17:08:13 +0530

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-4xl font-bold text-blue-600">
+      Tailwind is working! 🚀
+    </div>
+  );
+}
+
+export default App;
